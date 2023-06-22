3df89b569272fec4134af3e1b058519c9dd08f6f
Daniel Victor <4viks98@gmail.com>
2023-06-22 12:02:16 -0400

    added serial monitor and pinMotion

--- hangar_door/hangar_door.ino
+++ hangar_door/hangar_door.ino
@@ -2,25 +2,35 @@
 //
 int pinSensor =2;
 int pinLed =12;
+int pinMotion = 7;
 int pinBuzzer =13;
 int pirSensor =0;
 
 void setup()
 {
+  Serial.begin(115200);
+  
   pinMode(pinSensor, INPUT);
   pinMode(pinLed, OUTPUT);
   pinMode(pinBuzzer, OUTPUT);
+  pinMode(pinMotion, OUTPUT);
   
 }
 
 void loop()
 {
+  Serial.println(digitalRead(pinMotion));
   
   pirSensor = digitalRead(pinSensor);
   if (pirSensor == HIGH)
   {
   
     digitalWrite(pinLed, HIGH);
+    
+    // Tell Uno2 there was motion and kick off audio
+    digitalWrite(pinMotion, HIGH);
+    Serial.println(digitalRead(pinMotion));
+    
     tone(pinBuzzer, 1000, 500);
   
   }
@@ -28,7 +38,8 @@ void loop()
   else {
     
     digitalWrite(pinLed, LOW);
+    digitalWrite(pinMotion, LOW);
   }
   
-  delay(10);
+  delay(100);
 }
