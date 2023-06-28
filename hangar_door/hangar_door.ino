d3c2afcd3cd1cd7ea2741a37b13f24cc3b8665ef
Daniel Victor <4viks98@gmail.com>
2023-06-28 09:05:40 -0400

    post int testing - set motion low

--- hangar_door/hangar_door.ino
+++ hangar_door/hangar_door.ino
@@ -3,8 +3,8 @@
 // set motion pin high and is monitored by the LED device.
 
 int pinBreakBeam = 2;
-int pinMusicStatus = 4;
 int pinLed = 12;
+int pinMusicStatusFeedback = 7;
 int pinMotionSignal = 8;
 int beamSensorState = 0;
 int beamSensorLastState = 0;
@@ -13,14 +13,13 @@ void setup(){
   Serial.begin(115200);
 
   pinMode(pinLed, OUTPUT);
-  pinMode(pinBuzzer, OUTPUT);
   pinMode(pinMotionSignal, OUTPUT);
   pinMode(pinBreakBeam, INPUT_PULLUP);
   //Setup interrupt for break beam sensor
   attachInterrupt (digitalPinToInterrupt (2), detected, FALLING);   
 
   // Music status
-  pinMode(pinMusicStatus, INPUT);
+  pinMode(pinMusicStatusFeedback, INPUT);
   
 }
 
@@ -37,9 +36,12 @@ void detected(){
 void loop()
 {  
   //Check for the audio complete signal going high, then turn the LED off
-  while(digitalRead(pinMusicStatus) == HIGH){
+  while(digitalRead(pinMusicStatusFeedback) == HIGH){
     //music is done, turn the LED off
     digitalWrite(pinLed, LOW);
+    digitalWrite(pinMotionSignal, LOW);
+  
+    
 
   }
 }
