67e6944652415b34cd5291159b8caf7849acd763
Daniel Victor <4viks98@gmail.com>
2023-06-27 20:21:48 -0400

    unused code cleanup

--- hangar_door/hangar_door.ino
+++ hangar_door/hangar_door.ino
@@ -6,12 +6,8 @@ int pinBreakBeam = 2;
 int pinMusicStatus = 4;
 int pinLed = 12;
 int pinMotionSignal = 8;
-int pinBuzzer = 13;
 int beamSensorState = 0;
 int beamSensorLastState = 0;
-int ledDelayTime = 15000; //15000 seconds
-unsigned long previousTime = 0;
-
 
 void setup(){
   Serial.begin(115200);
@@ -19,14 +15,12 @@ void setup(){
   pinMode(pinLed, OUTPUT);
   pinMode(pinBuzzer, OUTPUT);
   pinMode(pinMotionSignal, OUTPUT);
-
   pinMode(pinBreakBeam, INPUT_PULLUP);
   //Setup interrupt for break beam sensor
   attachInterrupt (digitalPinToInterrupt (2), detected, FALLING);   
 
   // Music status
   pinMode(pinMusicStatus, INPUT);
-
   
 }
 
